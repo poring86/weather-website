a45fa1fde54127cd560fa3c123bac847041f4e11
Matheus <matheuslino86a@gmail.com>
2020-05-31 16:04:41 -0400

    remove console.log

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,3 @@
-console.log('JS file loaded!')
-
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
